2bd926a7e9289f2b8e3e545f2b4c592eb18d7bbe
shogo.kusumure <shogo9180@gmail.com>
2020-07-25 21:41:31 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './calculator.css'
 import * as serviceWorker from './serviceWorker';
+
+class Button extends React.Component {
+  renderButton(i){
+    return <Counter value={i} />
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    }
+  }
+
+  handleClick(i){
+    let currentCount = this.state.value
+    currentCount += 1
+
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = currentCount;
+    msg.lang = 'ja-JP';
+    window.speechSynthesis.speak(msg);
+
+    this.setState({value: currentCount})
+  }
+
+  handleReset(i){
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = 'リセットしました。';
+    msg.lang = 'ja-JP';
+    window.speechSynthesis.speak(msg);
+
+    this.setState({value: 0})
+  }
+
+  render() {
+    return(
+      <div>
+        {this.renderButton(this.state.value)}
+        <div className="button" onClick={() => this.handleClick()}>PUSH!!</div>
+        <div className="reset" onClick={() => this.handleReset()}>RESET</div>
+      </div>
+    )
+  }
+}
+
+class Counter extends React.Component {
+  render() {
+    return (
+      <div className="display">
+        <p>現在の回数…</p>
+        {this.props.value}<span>回</span>
+      </div>
+    )
+  }
+}
+
+class Calculator extends React.Component {
+  render() {
+    return(
+      <div className="calculator">
+        <Button />
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Calculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
